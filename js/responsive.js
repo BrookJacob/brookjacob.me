e4a04eb2b7149b910b9e136c7bd806fd26ef8832
BrookJacob <brookjacob@gmail.com>
2018-08-21 17:51:55 -0400

    wrapper now automatically resizes according to page

--- js/responsive.js
+++ js/responsive.js
@@ -8,4 +8,13 @@ if (document.addEventListener) {
 function responsive() {
     var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    var heightSub = height * .5;
+    var widthSub = width * .5;
+
+
+    var marginHeight = (height - heightSub)/2;
+    var marginWidth = (width - widthSub)/2;
+
+    document.getElementById("wrapper").style.margin = marginHeight + "px " + marginWidth + "px"; 
 }
