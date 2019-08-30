bc79029700109040d726d0c1bf78ce1d59ffe897
Jacob Brook <brookjacob@gmail.com>
2019-08-30 00:39:00 -0600

    fixed uncaught error type

--- background.js
+++ background.js
@@ -2,9 +2,8 @@ function getHTMLString() {
     let windowWidth = window.innerWidth;
     let quotient = Math.floor(windowWidth/150);
     let html = "<div class=\"scroll\"></div>";
-    let i = 0;
 
-    for (i = 0; i < (quotient - 1); i += 1) {
+    for (let i = 0; i < (quotient - 1); i++) {
         html += "<div class=\"scroll\"></div>";
     }
     return html;
@@ -14,33 +13,34 @@ function setSize(){
     let divisor = Math.floor(windowWidth/150.00);
     let result = (windowWidth/divisor);
 
-    let elements = document.body.getElementsByClassName("scroll");
-    let element;
+    var elements = document.body.getElementsByClassName("scroll");
+    var element;
+    
     for (element of elements) {
         element.style.width = result + "px";
     }
 }
 
 function createBackground(html) {
-    let wrapper = document.getElementById("wrapper");
+    var wrapper = document.getElementById("wrapper");
     wrapper.innerHTML = "";
-    let fragment = document.createDocumentFragment();
-    let temp = document.createElement("div");
+    var fragment = document.createDocumentFragment();
+    var temp = document.createElement("div");
 
     temp.innerHTML = html;
     while(temp.firstChild) {
         fragment.appendChild(temp.firstChild);
     }
+    
     return fragment;
 }
 function runAll() {
-    let i = 0;
-    let fragment = createBackground(getHTMLString());
-    let target = document.getElementById("wrapper");
+    var fragment = createBackground(getHTMLString());
+    var target = document.getElementById("wrapper");
     target.insertBefore(fragment, target.childNodes[0]);
     setSize();
-    let scrolls = document.getElementsByClassName("scroll");
-    for (i = 0; i < scrolls.length; i += 1){
+    var scrolls = document.getElementsByClassName("scroll");
+    for (let i = 0; i < scrolls.length; i++){
         scrolls[i].addEventListener("mouseover", function(){
             scrolls[i].style.animation = "100s scroll infinite linear reverse";
         });
